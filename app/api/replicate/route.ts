7515a2fd268c3eb57c1da3fab95923fe601e3d9a
kotadd <nishinaka4business@gmail.com>
2023-06-04 21:47:36 +0900

    created restore old image

---
+++ app/api/replicate/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import Replicate from "replicate";
+
+export async function POST(request: Request) {
+  // 1. Get the request data (in JSON format) from the client
+  const req = await request.json();
+
+  // 2. Initialize the replicate object with our Replicate API token
+  const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_TOKEN as string,
+  });
+
+  // 3. Set the model that we're about to run
+  const model =
+    "microsoft/bringing-old-photos-back-to-life:c75db81db6cbd809d93cc3b7e7a088a351a3349c9fa02b6d393e35e0d51ba799";
+
+  // 4. Set the input image which is the image we uploaded from the client
+  const input = {
+    image: req.image,
+    with_scratch: true,
+  };
+
+  // 5. Run the Replicate's model (to remove background) and get the output image
+  const output = await replicate.run(model, { input });
+
+  // 6. Check if output is NULL then return error back to the client
+  if (!output) {
+    console.log("Something went wrong");
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+
+  // 7. Otherwise, we show output in the console (SERVER side)
+  // and return output back to the client
+  console.log("OUTPUT: ", output);
+  return NextResponse.json({ output }, { status: 201 });
+}
